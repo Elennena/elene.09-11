a0402e762f993094ee0467916885761d60a94b64
AnriSichinava <Anrisichinava10@gmail.com>
2023-11-09 10:47:02 +0400

    int

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputElement = document.getElementByTagName(qualifiedName) 'input')[0];
+const pElement=document,getElementsByTagName(qualifiedName 'p') [0];
+inputElement.addEventListener(type: 'mousemove', listener: function () {
+pElement.Style.border ="ipx solid black";
+pELement.style.display="inline";
+pelement.style.margin= "10px";
+});
